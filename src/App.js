7bfe2ea8f3b5b563db3d3960da81d28233587a2e
Bohdansam <bogdanbisnes3@gmail.com>
2026-04-24 19:25:33 +0200

    fix unused imports for netlify build

--- src/App.js
+++ src/App.js
@@ -3,12 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import PortfolioSection from './components/ProductShowCase';
-import Footer from './components/Footer';
-import ProductDetailPage from './components/ProductDetailPage';
-import Basket from './components/Basket'; 
-import { BasketProvider } from './BasketContext'; 
 import AboutSection from './components/AboutSection';
 import ContactSection from './components/ContactSection';
+import { BasketProvider } from './BasketContext';
 
 function App() {
   return (
@@ -16,7 +13,6 @@ function App() {
       <Router>
         <div className="relative text-white font-montserrat">
 
-          {/* BACKGROUND */}
           <div className="fixed inset-0 -z-10 bg-gradient-to-br from-[#0c0618] via-[#140a24] to-black" />
           <div className="fixed w-[600px] h-[600px] bg-purple-600/20 blur-[150px] rounded-full top-[-200px] left-1/2 -translate-x-1/2 -z-10" />
 
@@ -31,8 +27,6 @@ function App() {
                 <ContactSection />
               </>
             }/>
-
-        
           </Routes>
 
         </div>
